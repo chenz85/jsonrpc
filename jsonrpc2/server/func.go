815fb09edc78ca70ba9bebc0c69f1dd4bd1a6938
Chen Zhen <cz.worker@gmail.com>
2018-08-29 14:19:10 +0800

    impl RPCMethod.Invoke() & RPCMethod.InvokeA()

--- jsonrpc2/server/func.go
+++ jsonrpc2/server/func.go
@@ -17,12 +17,37 @@ type RPCMethod struct {
 func (m *RPCMethod) Invoke() (result interface{}, err object.Err) {
 	if m.rf.Type().NumIn() != 0 {
 		err = object.ErrMethod_ParamsNumNotMatch
+	} else {
+		var result_vals = m.rf.Call(nil)
+		result = m.return_values(result_vals)
 	}
 	return
 }
 func (m *RPCMethod) InvokeA(params []interface{}) (result interface{}, err object.Err) {
 	if m.rf.Type().NumIn() != len(params) {
 		err = object.ErrMethod_ParamsNumNotMatch
+	} else {
+		var param_vals = make([]reflect.Value, len(params))
+		for i, p := range params {
+			param_vals[i] = reflect.ValueOf(p)
+		}
+		var result_vals = m.rf.Call(param_vals)
+		result = m.return_values(result_vals)
+	}
+	return
+}
+
+func (m *RPCMethod) return_values(vals []reflect.Value) (result interface{}) {
+	if result_num := m.rf.Type().NumOut(); result_num == 0 {
+		result = nil
+	} else if result_num == 1 {
+		result = vals[0].Interface()
+	} else {
+		var results = make([]interface{}, result_num)
+		for i, rv := range vals {
+			results[i] = rv.Interface()
+		}
+		result = results
 	}
 	return
 }
