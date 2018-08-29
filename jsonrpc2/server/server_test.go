e4d6202929e9dbe1d7f2c541d0dcd08a2d076477
Chen Zhen <cz.worker@gmail.com>
2018-08-29 16:55:58 +0800

    add more test case in TestHandleRequest()

--- jsonrpc2/server/server_test.go
+++ jsonrpc2/server/server_test.go
@@ -18,16 +18,103 @@ func TestHandleRequest(t *testing.T) {
 			"{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": [42, 23], \"id\": 1}",
 			"{\"jsonrpc\": \"2.0\", \"result\": 19, \"id\": 1}",
 		},
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": [23, 42], \"id\": 2}",
+			"{\"jsonrpc\": \"2.0\", \"result\": -19, \"id\": 2}",
+		},
+		// {
+		// 	"{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": {\"subtrahend\": 23, \"minuend\": 42}, \"id\": 3}",
+		// 	"{\"jsonrpc\": \"2.0\", \"result\": 19, \"id\": 3}",
+		// },
+		// {
+		// 	"{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": {\"minuend\": 42, \"subtrahend\": 23}, \"id\": 4}",
+		// 	"{\"jsonrpc\": \"2.0\", \"result\": 19, \"id\": 4}",
+		// },
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": \"update\", \"params\": [1,2,3,4,5]}",
+			"",
+		},
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": \"foobar\"}",
+			"",
+		},
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": \"foobar\", \"id\": \"1\"}",
+			"{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": \"1\"}",
+		},
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": \"foobar, \"params\": \"bar\", \"baz]",
+			"{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32700, \"message\": \"Parse error\"}, \"id\": null}",
+		},
+		{
+			"{\"jsonrpc\": \"2.0\", \"method\": 1, \"params\": \"bar\"}",
+			"{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null}",
+		},
+		{
+			`[
+				{\"jsonrpc\": \"2.0\", \"method\": \"sum\", \"params\": [1,2,4], \"id\": \"1\"},
+				{\"jsonrpc\": \"2.0\", \"method\"
+			]`,
+			"{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32700, \"message\": \"Parse error\"}, \"id\": null}",
+		},
+		{
+			"[]",
+			"{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null}",
+		},
+		{
+			"[1]",
+			"[{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null}]",
+		},
+		{
+			"[1,2,3]",
+			`[
+				{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null},
+				{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null},
+				{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null}
+			]`,
+		},
+		{
+			`[
+				{\"jsonrpc\": \"2.0\", \"method\": \"sum\", \"params\": [1,2,4], \"id\": \"1\"},
+				{\"jsonrpc\": \"2.0\", \"method\": \"notify_hello\", \"params\": [7]},
+				{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": [42,23], \"id\": \"2\"},
+				{\"foo\": \"boo\"},
+				{\"jsonrpc\": \"2.0\", \"method\": \"foo.get\", \"params\": {\"name\": \"myself\"}, \"id\": \"5\"},
+				{\"jsonrpc\": \"2.0\", \"method\": \"get_data\", \"id\": \"9\"} 
+			]`,
+			`[
+				{\"jsonrpc\": \"2.0\", \"result\": 7, \"id\": \"1\"},
+				{\"jsonrpc\": \"2.0\", \"result\": 19, \"id\": \"2\"},
+				{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32600, \"message\": \"Invalid Request\"}, \"id\": null},
+				{\"jsonrpc\": \"2.0\", \"error\": {\"code\": -32601, \"message\": \"Method not found\"}, \"id\": \"5\"},
+				{\"jsonrpc\": \"2.0\", \"result\": [\"hello\", 5], \"id\": \"9\"}
+			]`,
+		},
+		{
+			`[
+				{\"jsonrpc\": \"2.0\", \"method\": \"notify_sum\", \"params\": [1,2,4]},
+				{\"jsonrpc\": \"2.0\", \"method\": \"notify_hello\", \"params\": [7]}
+			]`,
+			"",
+		},
 	}
 
 	server.HandleFunc("subtract", func(a, b float64) float64 {
 		return a - b
 	})
 
-	for _, value := range values {
+	server.HandleFunc("update", func(a, b, c, d, e float64) {
+
+	})
+
+	for i, value := range values {
 		resp := server.HandleRequest([]byte(value.request))
-		if resp[0] != value.response[0] {
-			t.Errorf("response not match (type): Got: %s, Expect: %s", string(resp), value.response)
+		if len(resp) == 0 && value.response == "" {
+			continue
+		} else if len(resp) == 0 && value.response != "" || len(resp) != 0 && value.response == "" {
+			t.Errorf("response not match (#%d) (type): Got: %s, Expect: %s", i, string(resp), value.response)
+		} else if resp[0] != value.response[0] {
+			t.Errorf("response not match (#%d) (type): Got: %s, Expect: %s", i, string(resp), value.response)
 		} else {
 			var a, b interface{}
 			var batch bool = resp[0] == '['
@@ -39,9 +126,9 @@ func TestHandleRequest(t *testing.T) {
 				b = make(map[string]interface{})
 			}
 			if ea, eb := json.Unmarshal(resp, &a), json.Unmarshal([]byte(value.response), &b); ea != nil || eb != nil {
-				t.Errorf("response not match (parse): Got: %s, Expect: %s", string(resp), value.response)
+				t.Errorf("response not match (#%d) (parse): Got: %s, Expect: %s", i, string(resp), value.response)
 			} else if !obj_cmpr(a, b) {
-				t.Errorf("response not match (cmpr): Got: %s, Expect: %s", string(resp), value.response)
+				t.Errorf("response not match (#%d) (cmpr): Got: %s, Expect: %s", i, string(resp), value.response)
 			}
 		}
 
